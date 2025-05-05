cc695fce6b530881edfc125e1509188fe28e4077
steveklausdev <steveklausdev@outlook.com>
2025-05-05 15:23:52 +0200

    ok

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
 <?php
 
-#$list = array{"test", "hallo", "whatsgood"};
-function myTest()
-{
-    static $counter = 0;
-    echo $counter;
-    $counter++;
-}
-
-myTest();
-myTest();
-myTest();
-myTest();
-
-echo "es hat geklappt";
-
-?>
+echo "this is bad";
